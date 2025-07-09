6619dfd814dba1c10f1dbb2c1d563b55f122bbd8
Israel Lissak <islissak@gmail.com>
2025-07-09 09:55:16 +0300

    Update header styling in App component for improved visual hierarchy

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,8 +9,8 @@ function App() {
 	return (
 		<div className="text-center">
 			<header className="flex flex-col items-center justify-center text-[calc(10px+2vmin)]">
-				<p className="text-5xl font-bold">🤺Beautiful Form🤺</p>
-				<div className="flex gap-4 mt-8 w-full items-center justify-center">
+				<p className="text-4xl font-bold mt-8">🤺Beautiful Form🤺</p>
+				<div className="flex gap-4 mt-4 w-full items-center justify-center">
 					{/* <UserForm /> */}
 					<SliceForm />
 				</div>
