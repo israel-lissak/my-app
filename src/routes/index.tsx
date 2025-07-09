d2b7b4d5bd1729f63862fdcbebd89b7a99377bb3
Israel Lissak <islissak@gmail.com>
2025-07-09 09:50:39 +0300

    Added 🤺

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,7 +9,7 @@ function App() {
 	return (
 		<div className="text-center">
 			<header className="flex flex-col items-center justify-center text-[calc(10px+2vmin)]">
-				<p className="text-5xl font-bold">Beautiful Form</p>
+				<p className="text-5xl font-bold">🤺Beautiful Form🤺</p>
 				<div className="flex gap-4 mt-8 w-full items-center justify-center">
 					{/* <UserForm /> */}
 					<SliceForm />
